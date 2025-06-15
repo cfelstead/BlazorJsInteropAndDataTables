5343b3f1a27d0940b031db99fcc84f93c1a16df2
Chris Felstead <chris.felstead@futuressport.com>
2025-06-15 14:37:24 +0100

    Add project files.

---
+++ DataTablesReloading/Components/Pages/Weather.razor.js
@@ -0,0 +1,9 @@
+﻿export function initializeDataTable(tableRef) {
+	console.log('init: ' + tableRef);
+	$('#' + tableRef).DataTable();
+}
+
+export function destroyDataTable(tableRef) {
+	console.log('dest: ' + tableRef);
+	$('#' + tableRef).DataTable().destroy();
+}
